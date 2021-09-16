b98fd25558ab5c7d49198fd41341dfdcf5ccd640
sanishchirayath1 <sanishchirayath@gmail.com>
2021-09-16 16:06:47 +0530

    failure page

--- app.py
+++ app.py
@@ -2,14 +2,22 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+SPORTS = [
+    "Dodgeball",
+    "Cricket",
+    "Soccer",
+    "Hockey",
+    "Chess"
+]
+
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", sports=SPORTS)
 
 
 @app.route("/register", methods=["POST", "GET"])
 def register():
-    if not request.form.get("name") or not request.form.get("sport"):
+    if not request.form.get("name") or request.form.get("sport") not in SPORTS:
         return render_template("failure.html")
     return render_template("success.html")
